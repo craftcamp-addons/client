6c2af10ba7f5602997225713bbee45dc6fd3031c
mrmamongo <mrmamongo@engee.cloud>
2023-04-21 17:37:33 +0300

    Добавлен флаг headless, работает только на linux

--- parser/parser.py
+++ parser/parser.py
@@ -3,13 +3,13 @@ import logging
 from pathlib import Path
 from sys import platform
 from typing import Protocol, Any
+from config import settings
 
-if platform == "linux":
+if platform == "linux" and not settings.selenium.headless:
     import xvfbwrapper
 from selenium import webdriver
 from selenium.common import WebDriverException
 
-from config import settings
 from database import get_session, get_actual_number, Number
 from parser.basic_log_in_impl import BasicLogInImpl
 from parser.basic_parser_impl import BasicParserImpl
@@ -38,72 +38,75 @@ class Parser:
     logger: logging.Logger = logging.getLogger("Parser")
 
     async def parse(self):
-        if self.driver is None:
-            if platform == 'linux':
-                self.display = xvfbwrapper.Xvfb()
-                self.display.start()
-            try:
-                options = webdriver.ChromeOptions()
-                options.add_argument("--disable-dev-shm-usage")
-                options.add_argument("--allow-profiles-outside-user-dir")
-                options.add_experimental_option("detach", True)
-                options.add_experimental_option("excludeSwitches", ["enable-logging"])
-                options.add_argument("--enable-profile-shortcut-manager")
-                chromedriver_data_dir: Path = Path(
-                    settings.selenium.chromedriver_data_dir
-                ).absolute()
-                options.add_argument(
-                    f'--user-data-dir={chromedriver_data_dir / "user"}'
-                )
-                options.add_argument("--profile-directory=Profile 1")
-
-                chromedriver_data_dir: Path = Path(
-                    settings.selenium.chromedriver_path
-                ).absolute()
-                self.driver = webdriver.Chrome(
-                    executable_path=(
-                        chromedriver_data_dir
-                        if platform != "win32" and (".exe" not in str(chromedriver_data_dir))
-                        else str(chromedriver_data_dir) + ".exe"
-                    ),
-                    options=options,
-                )
-            except WebDriverException as e:
-                self.logger.error(e)
-                raise RuntimeError("Не удалось запустить chromedriver")
-            self.parser = BasicParserImpl(
-                self.driver,
-                settings.parser.url,
-                settings.parser.webdriver_timeout,
-                settings.parser.photos_dir,
-            )
-            self.user_logger = BasicLogInImpl(self.driver)
-
-        async with get_session() as session:
-            try:
-                while not self.whatsapp_logged_in:
-                    # В headless режиме сделать авторизацию через бота
-                    self.logger.info(
-                        "Пользователь не авторизован. Ожидаю авторизацию..."
+        try:
+            if self.driver is None:
+                if platform == 'linux' and not settings.selenium.headless:
+                    self.display = xvfbwrapper.Xvfb()
+                    self.display.start()
+                try:
+                    options = webdriver.ChromeOptions()
+                    options.add_argument("--disable-dev-shm-usage")
+                    options.add_argument("--allow-profiles-outside-user-dir")
+                    options.add_experimental_option("detach", True)
+                    options.add_experimental_option("excludeSwitches", ["enable-logging"])
+                    options.add_argument("--enable-profile-shortcut-manager")
+                    chromedriver_data_dir: Path = Path(
+                        settings.selenium.chromedriver_data_dir
+                    ).absolute()
+                    options.add_argument(
+                        f'--user-data-dir={chromedriver_data_dir / "user"}'
                     )
-                    self.whatsapp_logged_in = self.user_logger.log_in(
-                        settings.selenium.log_in_timeout
+                    options.add_argument("--profile-directory=Profile 1")
+
+                    chromedriver_data_dir: Path = Path(
+                        settings.selenium.chromedriver_path
+                    ).absolute()
+                    self.driver = webdriver.Chrome(
+                        executable_path=(
+                            chromedriver_data_dir
+                            if platform != "win32" and (".exe" not in str(chromedriver_data_dir))
+                            else str(chromedriver_data_dir) + ".exe"
+                        ),
+                        options=options,
                     )
+                except WebDriverException as e:
+                    self.logger.error(e)
+                    raise RuntimeError("Не удалось запустить chromedriver")
+                self.parser = BasicParserImpl(
+                    self.driver,
+                    settings.parser.url,
+                    settings.parser.webdriver_timeout,
+                    settings.parser.photos_dir,
+                )
+                self.user_logger = BasicLogInImpl(self.driver)
 
-                actual_number: Number | None = await get_actual_number(session)
-                if actual_number is None:
-                    return
+            async with get_session() as session:
+                try:
+                    while not self.whatsapp_logged_in:
+                        # В headless режиме сделать авторизацию через бота
+                        self.logger.info(
+                            "Пользователь не авторизован. Ожидаю авторизацию..."
+                        )
+                        self.whatsapp_logged_in = self.user_logger.log_in(
+                            settings.selenium.log_in_timeout
+                        )
 
-                self.logger.info(f"Парсинг номера: {actual_number.number}")
-                await self.parser.parse(actual_number)
-                await session.commit()
-            except Exception as e:
-                self.logger.error(e)
-                await session.rollback()
-                self.driver.quit()
-                self.driver = None
-                if platform == "linux":
-                    self.display.stop()
+                    actual_number: Number | None = await get_actual_number(session)
+                    if actual_number is None:
+                        return
+
+                    self.logger.info(f"Парсинг номера: {actual_number.number}")
+                    await self.parser.parse(actual_number)
+                    await session.commit()
+                except Exception as e:
+                    self.logger.error(e)
+                    await session.rollback()
+                    self.driver.quit()
+                    self.driver = None
+                    if platform == "linux":
+                        self.display.stop()
+        except Exception as e:
+            self.logger.error(e)
 
     async def start_parsing(self):
         while True:
@@ -121,6 +124,5 @@ class Parser:
         try:
             await parser.start_parsing()
         finally:
-            print("Отъебнуло")
             if parser.driver is not None:
                 parser.driver.quit()
